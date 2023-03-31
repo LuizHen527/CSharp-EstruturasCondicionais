3d84c09fad119f992287dc514ee1cc01a48361b0
Luiz <38606125814@info.local>
2023-03-31 08:14:01 -0300

    terminei execicio 3

--- back-end/EstruturasCondicionais/exercicio-3/Program.cs
+++ back-end/EstruturasCondicionais/exercicio-3/Program.cs
@@ -3,10 +3,10 @@
 // - Insira a terceira medida do triangulo:
 // - If medida1 == medida2 && medida2 == medida3,
 // Esse e um Triângulo Equilátero
-// - Else if (medida1 == medida2 || medida1 == medida3 || medida3 == medida2)
-// Esse e um Triângulo Isóscele
+// - Else if (medida1 == medida2) || (medida1 == medida3) || (medida3 == medida2)
+// Esse e o triangulo Isosceles
 // - Else 
-// Esse e um Triângulo Escaleno
+// Esse e um triangulo Escaleno
 
 float medida1;
 float medida2;
@@ -22,3 +22,19 @@ medida2 = float.Parse(Console.ReadLine());
 
 Console.WriteLine($"Insira a terceira medida do triangulo:");
 
+medida3 = float.Parse(Console.ReadLine());
+
+if ((medida1 == medida2) && (medida2 == medida3))
+{
+    Console.WriteLine("Esse é um Triângulo Equilátero");
+}
+
+else if ((medida1 == medida2) || (medida1 == medida3) || (medida3 == medida2))
+{
+    Console.WriteLine("Esse é um Triângulo Isósceles");
+}
+
+else 
+{
+    Console.WriteLine($"Esse é um Triângulo Escaleno");
+}
